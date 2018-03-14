aafc303c145d6c109c5a52c6d46cf9170836886f
paiforsyth <paiforsyth@hotmail.com>
2018-03-14 10:24:32 -0400

    edit scripts

--- sh/medium_se_resnext_l0.sh
+++ sh/medium_se_resnext_l0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=20000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-24:00
 #SBATCH --output=%N-%jl0reg_wide.out
 module purge
