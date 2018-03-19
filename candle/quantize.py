7e0890af79b8427c061fdda5e0bfc7717d1bf253
paiforsyth <paiforsyth@hotmail.com>
2018-03-19 13:29:38 -0400

    more version issues

--- candle/quantize.py
+++ candle/quantize.py
@@ -71,7 +71,7 @@ class BinaryTanhFunction(ag.Function):
     def backward(ctx, grad_output):
         input, = ctx.saved_tensors
         output = grad_output
-        if  len( torch.abs(input)> 0)>0: #version issue
+        if  len( torch.abs(input)> 0)>0 and len(output)>0: #version issue
             output[torch.abs(input)>1]=0
         
         return output
