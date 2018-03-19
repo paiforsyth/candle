3e141db874b13eea85ac51f31cae6ebaebe85aad
paiforsyth <paiforsyth@hotmail.com>
2018-03-18 22:21:52 -0400

    shuffle script

--- sh/basic_se_shufflenet.sh
+++ sh/basic_se_shufflenet.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=20000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-00:10
+#SBATCH --time=3-00:00
 #SBATCH --output=%N-%jshufflenet.out
 module purge
 module load python/3.6.3
