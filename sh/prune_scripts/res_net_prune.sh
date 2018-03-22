c110a948fe5b728682cf905ef6384a49c81ee98d
paiforsyth <paiforsyth@hotmail.com>
2018-03-22 11:58:54 -0400

    typo

--- sh/prune_scripts/res_net_prune.sh
+++ sh/prune_scripts/res_net_prune.sh
@@ -1,5 +1,5 @@
 ARGFILE=./sh/argfiles/medium_res_net
-RESUME_FILENAME= 21_March_2018_Wednesday_23_40_27anneal-medium_endofcycle_checkpoint_3
+RESUME_FILENAME=21_March_2018_Wednesday_23_40_27anneal-medium_endofcycle_checkpoint_3
 SAVE_PREFIX=res_net_anneal_medium
 PRUNE_PCT=10
 REPORT_FILENAME=./reports/medium_res_net_prune_$PRUNE_PCT
