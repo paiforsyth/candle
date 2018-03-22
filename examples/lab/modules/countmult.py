a671f442ddf64305ced98d158459d31eb6b48138
paiforsyth <paiforsyth@hotmail.com>
2018-03-22 17:37:09 -0400

    typo

--- examples/lab/modules/countmult.py
+++ examples/lab/modules/countmult.py
@@ -46,6 +46,6 @@ def count_approx_multiplies(layer,img_h,img_w, input_channels):
             sublayer_mult, sublayer_channels, sublayer_h, sublayer_w = count_approx_multipliies(sublayer, img_h = sublayer_h,img_w = sublayer_w,input_channels=  sublayer_channels)
             total += sublayer_mult
         return total, sublayer_channels, sublayer_h, sublayer_w
-    except: TypeError:
+    except TypeError:
         pass
     raise TypeError("Unable to compute multiplies for layer of type " + layer.__class__.__name__)
