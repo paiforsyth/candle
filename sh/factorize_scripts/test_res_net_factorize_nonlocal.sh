7df68bdfa80f78960f581ef3ddf48eea0c838fdf
paiforsyth <paiforsyth@hotmail.com>
2018-03-26 16:52:03 -0400

    script

--- sh/factorize_scripts/test_res_net_factorize_nonlocal.sh
+++ sh/factorize_scripts/test_res_net_factorize_nonlocal.sh
@@ -1,8 +1,15 @@
-
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=20000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=0-30:00
+#SBATCH --output=%N-%jfactortest.out
+module purge
+module load python/3.6.3
+source ~/env1/bin/activate
 ARGFILE=./sh/argfiles/ultrasmall
 RESUME_FILENAME=21_March_2018_Wednesday_23_40_27anneal-medium_endofcycle_checkpoint_3
-SAVE_PREFIX=res_net_smallm
-PRUNE_PCT=10
+SAVE_PREFIX=factor_test
 REPORT_FILENAME=./reports/small_re_factorize
 
 python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --factorize_trained  --proxy_context_type=stdfactorize_context --cuda 
