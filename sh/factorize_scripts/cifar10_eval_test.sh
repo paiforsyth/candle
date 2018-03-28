c48fa34e10fee8b95b5195f8ffecf1de2865731e
paiforsyth <paiforsyth@hotmail.com>
2018-03-28 12:44:10 -0400

    use correct args

--- sh/factorize_scripts/cifar10_eval_test.sh
+++ sh/factorize_scripts/cifar10_eval_test.sh
@@ -7,7 +7,7 @@ SVD_PROP=0.9
 
 #python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --factorize_trained  --proxy_context_type=stdfactorize_context --factorize_svd_rank_prop=$SVD_PROP --cuda 
 
-python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}   --use_custom_test_data_file --custom_test_data_file="./local_data/cifar10/cifar-10-batches-py/test_batch" --test_report_filename=$REPORT_FILENAME  --cuda
+python -m examples.lab $(cat $ARGFILE) --proxy_context_type=stdfactorize_context  --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}   --use_custom_test_data_file --custom_test_data_file="./local_data/cifar10/cifar-10-batches-py/test_batch" --test_report_filename=$REPORT_FILENAME  --cuda
 
 python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar/train_data --validate_fr_truthfiletype="pickle_dict"
 
