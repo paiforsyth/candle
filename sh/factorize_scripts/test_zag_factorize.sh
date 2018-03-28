13a76489a986211e8ea0a753ef1cceef409594f3
paiforsyth <paiforsyth@hotmail.com>
2018-03-28 13:10:33 -0400

    correct res file

--- sh/factorize_scripts/test_zag_factorize.sh
+++ sh/factorize_scripts/test_zag_factorize.sh
@@ -5,7 +5,7 @@ SAVE_PREFIX=zag_svd
 REPORT_FILENAME=./reports/small_re_factorize
 SVD_PROP=0.9
 
-python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --factorize_trained  --proxy_context_type=stdfactorize_context --factorize_svd_rank_prop=$SVD_PROP --cuda 
+python -m examples.lab $(cat $ARGFILE) --res_file=${RESUME_FILENAME} --resume_mode=standard --save_prefix=$SAVE_PREFIX --factorize_trained  --proxy_context_type=stdfactorize_context --factorize_svd_rank_prop=$SVD_PROP --cuda 
 
 
 python -m examples.lab $(cat $ARGFILE) --proxy_context_type=stdfactorize_context  --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard    --res_file=${RESUME_FILENAME}_svd_factorize_$SVD_PROP  --use_custom_test_data_file --custom_test_data_file="./local_data/cifar10/cifar-10-batches-py/test_batch" --test_report_filename=$REPORT_FILENAME  --cuda
