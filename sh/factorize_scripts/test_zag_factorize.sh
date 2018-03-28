b51abfd86ee1a67f67b2b494a239e72dcacfcad3
paiforsyth <paiforsyth@hotmail.com>
2018-03-28 19:34:31 -0400

    retrain

--- sh/factorize_scripts/test_zag_factorize.sh
+++ sh/factorize_scripts/test_zag_factorize.sh
@@ -2,7 +2,7 @@
 ARGFILE=./sh/argfiles/zag_small
 RESUME_FILENAME=28_March_2018_Wednesday_16_20_08zag_for_factorize_checkpoint_0
 SAVE_PREFIX=zag_svd
-SVD_PROP=0.9
+SVD_PROP=0.6
 REPORT_FILENAME=./reports/zag_factorize_${SVD_PROP}
 
 python -m examples.lab $(cat $ARGFILE) --squeezenet_zag_dont_bypass_last   --res_file=${RESUME_FILENAME} --resume_mode=standard --load_nonstrict --save_prefix=$SAVE_PREFIX --factorize_trained  --proxy_context_type=stdfactorize_context --create_svd_rank_prop=1 --factorize_svd_rank_prop=$SVD_PROP --cuda 
@@ -14,4 +14,4 @@ python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME 
 
 #retrain
 
-python -m examples.lab $(cat $ARGFILE)  --squeezenet_zag_dont_bypass_last  --factorize_use_factors --create_svd_rank_prop=$SVD_PROP --proxy_context_type=stdfactorize_context  --save_prefix=${SAVE_PREFIX}_svd_prop_{SVD_PROP}_retrained  --resume_mode=standard    --res_file=${RESUME_FILENAME}_svd_factorize_$SVD_PROP --load_nonstrict    --cuda
+python -m examples.lab $(cat $ARGFILE)  --squeezenet_zag_dont_bypass_last  --factorize_use_factors --create_svd_rank_prop=$SVD_PROP --proxy_context_type=stdfactorize_context  --save_prefix=${SAVE_PREFIX}_svd_prop_${SVD_PROP}_retrained  --resume_mode=standard    --res_file=${RESUME_FILENAME}_svd_factorize_$SVD_PROP --load_nonstrict    --cuda
