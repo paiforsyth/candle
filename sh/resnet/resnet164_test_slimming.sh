289994d9947e9ae010b37a1c91e654507c52dece
paiforsyth <paiforsyth@hotmail.com>
2018-04-04 10:47:39 -0400

    correct resume filename

--- sh/resnet/resnet164_test_slimming.sh
+++ sh/resnet/resnet164_test_slimming.sh
@@ -4,7 +4,7 @@ SAVE_PREFIX=res_net_164_slimming_08_test
 REPORT_FILENAME=./reports/res_net_164_slimming_08_test
 
 
-python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_$PRUNE_PCT    --test_report_filename=$REPORT_FILENAME  --cuda
+python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
 
 python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"
 
