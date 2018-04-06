63e680aac1b26dc7be8a3383617516effe716610
paiforsyth <paiforsyth@hotmail.com>
2018-04-06 09:33:23 -0400

    test basic prune

---
+++ sh/resnet/resnet164_test_basic_pruine03.sh
@@ -0,0 +1,12 @@
+ARGFILE=./sh/argfiles/resnet_164
+RESUME_FILENAME=05_April_2018_Thursday_19_38_46resnet164_basic_prune_03_retrain_most_recent
+SAVE_PREFIX=res_net_164_basic_prune_03_test
+REPORT_FILENAME=./reports/res_net_basic_prune_03_test
+
+
+python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
+
+python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"
+
+
+
