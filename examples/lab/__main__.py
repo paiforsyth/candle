08459e6e79b4d44ab7862ef041fde046d4427fab
paiforsyth <paiforsyth@hotmail.com>
2018-04-06 19:14:59 -0400

    autopruneunit

--- examples/lab/__main__.py
+++ examples/lab/__main__.py
@@ -96,6 +96,8 @@ def default_parser(parser=None):
     parser.add_argument("--prune_warmup_epochs", type=int, default=10)
     parser.add_argument("--prune_layer_mode", choices = ["by_layer", "global"], default= "by_layer")
     parser.add_argument("--prune_unit",type=int, default=1)
+    parser.add_argument("--autocalc_prune_unit",action="store_true")#overrided the above.  Uses as prune_unit what is neccesary to achieve the prune target in 10 epochs
+
 
     parser.add_argument("--enable_l0reg",action = "store_true")
     parser.add_argument("--l0reg_lambda", type=float, default =1.5 / 50000 )
