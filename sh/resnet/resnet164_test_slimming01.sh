a5d986ba1da9f6de7429643919c92ff2c4c66d57
paiforsyth <paiforsyth@hotmail.com>
2018-04-05 20:47:09 -0400

    test

--- sh/resnet/resnet164_test_slimming01.sh
+++ sh/resnet/resnet164_test_slimming01.sh
@@ -1,7 +1,7 @@
 ARGFILE=./sh/argfiles/resnet_164_slimming
-RESUME_FILENAME=03_April_2018_Tuesday_23_56_39resnet164_checkpoint_1
-SAVE_PREFIX=res_net_164_slimming_08_test
-REPORT_FILENAME=./reports/res_net_164_slimming_08_test
+RESUME_FILENAME=05_April_2018_Thursday_16_17_25resnet164_slim03_most_recent
+SAVE_PREFIX=res_net_164_slimming_01_test
+REPORT_FILENAME=./reports/res_net_164_slimming_01_test
 
 
 python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
