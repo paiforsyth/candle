e2b4a1a7b96ff7a925cef8b6759f8c6a29eb8edf
paiforsyth <paiforsyth@hotmail.com>
2018-04-09 12:34:03 -0400

    random test

---
+++ sh/resnet/newtests/resnet29_random/50_test.sh
@@ -0,0 +1,17 @@
+RUNTYPE=random
+PTARG=50
+NETTYPE=resnet29
+LAMBDA=00001
+ARGFILE=./sh/argfiles/${NETTYPE}_group
+RESUME_FILENAME=09_April_2018_Monday_06_46_51resnet29_random_random_50_fixedmaxpool_most_recent
+
+SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+
+
+python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
+
+python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"
+
+
+
