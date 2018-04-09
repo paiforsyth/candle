c33a950c3c71fa8fa9c623a6fde96a6330226900
paiforsyth <paiforsyth@hotmail.com>
2018-04-09 12:56:02 -0400

    typo

--- sh/resnet/newtests/resnet29_random/20_test.sh
+++ sh/resnet/newtests/resnet29_random/20_test.sh
@@ -3,7 +3,7 @@ PTARG=20
 NETTYPE=resnet29
 LAMBDA=00001
 ARGFILE=./sh/argfiles/${NETTYPE}_group
-RESUME_FILENAME=09_April_2018_Monday_04_38_55resnet29_random_random_20_fixedmaxpool_most_rec
+RESUME_FILENAME=09_April_2018_Monday_04_38_55resnet29_random_random_20_fixedmaxpool_most_recent
 SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
 REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
 
