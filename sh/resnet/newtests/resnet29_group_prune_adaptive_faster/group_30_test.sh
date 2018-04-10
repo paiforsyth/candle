d7aadaf9305bf03777f2d8dbc7474b6cb86d4f6d
paiforsyth <paiforsyth@hotmail.com>
2018-04-10 17:57:41 -0400

    wrong save file

--- sh/resnet/newtests/resnet29_group_prune_adaptive_faster/group_30_test.sh
+++ sh/resnet/newtests/resnet29_group_prune_adaptive_faster/group_30_test.sh
@@ -3,7 +3,8 @@ PTARG=30
 NETTYPE=resnet29
 LAMBDA=00001
 ARGFILE=./sh/argfiles/${NETTYPE}_group
-RESUME_FILENAME=10_April_2018_Tuesday_01_57_33resnet29_sensitivity_adaptive_group_prune_p30_lr05_test_fixedmaxpool_most_recent
+RESUME_FILENAME=10_April_2018_Tuesday_13_29_44resnet29_sensitivity_adaptive_group_prune_faster_p30_lr05_test_fixedmaxpool_most_recent
+
 
 SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
 REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
