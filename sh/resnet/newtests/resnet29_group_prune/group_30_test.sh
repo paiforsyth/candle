4d92a1b1d90c6ccdab86e6e89b81b519105619fe
paiforsyth <paiforsyth@hotmail.com>
2018-04-09 20:27:07 -0400

    test group prune

---
+++ sh/resnet/newtests/resnet29_group_prune/group_30_test.sh
@@ -0,0 +1,17 @@
+RUNTYPE=group_prune
+PTARG=30
+NETTYPE=resnet29
+LAMBDA=00001
+ARGFILE=./sh/argfiles/${NETTYPE}_group
+RESUME_FILENAME=09_April_2018_Monday_15_44_22resnet29_group_prune_standard_30_fixedmaxpool_most_recent
+
+SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+
+
+python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
+
+python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"
+
+
+
