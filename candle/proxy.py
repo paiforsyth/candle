1af2dbc11810e1340b10b078b55937e6f79864ec
paiforsyth <paiforsyth@hotmail.com>
2018-04-10 22:23:15 -0400

    pdb

--- candle/proxy.py
+++ candle/proxy.py
@@ -410,6 +410,7 @@ class ProxyLinear(ProxyLayer):
 
     def __repr__(self):
         s=super().__repr__()
+        import pdb; pdb.set_trace()
         s+= " outdim={}, indim={}. Weight_provider{}".format(self.weight_provider.root()[0].shape[0], self.weight_provider.root()[0].shape[1],self.weight_provider )
 
 
