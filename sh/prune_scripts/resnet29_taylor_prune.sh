14cbae7f7179994153f052e689917f8072f15266
paiforsyth <paiforsyth@hotmail.com>
2018-04-14 21:32:55 -0400

    debug

--- sh/prune_scripts/resnet29_taylor_prune.sh
+++ sh/prune_scripts/resnet29_taylor_prune.sh
@@ -1,18 +1,18 @@
-for PRUNE_PCT in   10; do
+for PRUNE_PCT in   10 10 10 40 50; do
 ARGFILE=./sh/argfiles/resnet29_group
 RESUME_FILENAME=11_April_2018_Wednesday_23_26_51_most_recent
 SAVE_PREFIX=res_net_29_group_prune
 REPORT_FILENAME=./reports/medium_res_net_164_naive_prune_$PRUNE_PCT
 
-python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT --cuda  --group_prune_strategy=taylor  #--output_level=warning --short_test_report
+python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT --cuda  --group_prune_strategy=taylor  --output_level=warning --short_test_report
 
-python  -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --test_report_filename=$REPORT_FILENAME  --cuda #--output_level=warning
+python  -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --test_report_filename=$REPORT_FILENAME  --cuda --output_level=warning
 
-python  -W ignore -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"  #--output_level=warning  --short_test_report
+python  -W ignore -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"  --output_level=warning  --short_test_report
 
 
 
-python  -W ignore -m examples.lab --save_prefix=$SAVE_PREFIX $(cat $ARGFILE) --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --count_multiplies    #--output_level=warning   --short_test_report
+python  -W ignore -m examples.lab --save_prefix=$SAVE_PREFIX $(cat $ARGFILE) --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --count_multiplies    --output_level=warning   --short_test_report
 
 
 done
