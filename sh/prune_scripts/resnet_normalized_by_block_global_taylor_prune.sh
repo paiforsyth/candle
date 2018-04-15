569a5821230f1a2949b3a671d90486250dd2e4e1
paiforsyth <paiforsyth@hotmail.com>
2018-04-15 19:29:47 -0400

    typo

--- sh/prune_scripts/resnet_normalized_by_block_global_taylor_prune.sh
+++ sh/prune_scripts/resnet_normalized_by_block_global_taylor_prune.sh
@@ -4,7 +4,7 @@ RESUME_FILENAME=11_April_2018_Wednesday_23_26_51_most_recent
 SAVE_PREFIX=res_net_29_group_prune
 REPORT_FILENAME=./reports/medium_res_net_164_naive_prune_$PRUNE_PCT
 
-python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT  --group_prune_strategy=taylor --prune_layer_mode=global --global_prune_normalization=by_block --cuda   --output_level=warning --short_test_report  a 
+python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT  --group_prune_strategy=taylor --prune_layer_mode=global --global_prune_normalization=by_block --cuda   --output_level=warning --short_test_report   
 
 python  -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --test_report_filename=$REPORT_FILENAME  --cuda  --output_level=warning
 
