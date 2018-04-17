abf49016409ca55a8f84af46d8f6c95f4e95f9bb
paiforsyth <paiforsyth@hotmail.com>
2018-04-17 16:47:25 -0400

    do

--- sh/prune_scripts/resnet_normalized_by_layer_global_taylor_prune.sh
+++ sh/prune_scripts/resnet_normalized_by_layer_global_taylor_prune.sh
@@ -1,4 +1,4 @@
-for PRUNE_PCT in 50 60 70 75 80 85 80 95 97
+for PRUNE_PCT in 50 60 70 75 80 85 80 95 97; do
  #   0 2 1 5 7 10 12 15 17 20 23 25 27 30 32 35 37 40 42 45 47 50 52 55 57 60 65 70; do
 ARGFILE=./sh/argfiles/resnet29_group
 RESUME_FILENAME=11_April_2018_Wednesday_23_26_51_most_recent
