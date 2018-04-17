1873e49eab095ee6b2e9fe3839dedde66e43bd39
paiforsyth <paiforsyth@hotmail.com>
2018-04-16 23:28:27 -0400

    noretrain slim

---
+++ sh/prune_scripts/resnet29_slim.sh
@@ -0,0 +1,16 @@
+for PRUNE_PCT in 0 2 5 7 10 12 15 17 20 22 25 27 30 33 35 37 40 42 45 47 50 52 55 57 60 65 70; do
+ARGFILE=./sh/argfiles/resnet29_slimming
+RESUME_FILENAME=08_April_2018_Sunday_14_50_19resnet29_slimming_50_lambda0.00001_fixedmaxpool_endofcycle_checkpoint_0 
+SAVE_PREFIX=res_net_29_group_prune
+REPORT_FILENAME=./reports/medium_res_net_164_naive_prune_$PRUNE_PCT
+
+python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT   --prune_layer_mode=global  --proxy_context_type=l1reg_context_slimming   --cuda  --output_level=warning --short_test_report
+
+python  -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_$PRUNE_PCT   --test_report_filename=$REPORT_FILENAME  --cuda --output_level=warning  --short_test_report
+
+python  -W ignore -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"  --output_level=warning  --short_test_report
+
+
+python  -W ignore -m examples.lab --save_prefix=$SAVE_PREFIX $(cat $ARGFILE) --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_$PRUNE_PCT   --count_multiplies    --output_level=warning   --short_test_report
+
+done
