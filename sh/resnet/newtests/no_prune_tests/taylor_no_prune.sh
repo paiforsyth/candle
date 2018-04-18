354bc2fbfdbee01c3873d6827dabf4790e2f86d9
paiforsyth <paiforsyth@hotmail.com>
2018-04-18 13:37:25 -0400

    taylor no prune

---
+++ sh/resnet/newtests/no_prune_tests/taylor_no_prune.sh
@@ -0,0 +1,16 @@
+RUNTYPE=taylor_no_prun
+PTARG=01
+NETTYPE=resnet29
+LAMBDA=00001
+ARGFILE=./sh/argfiles/${NETTYPE}_group
+RESUME_FILENAME=11_April_2018_Wednesday_23_26_51_most_recent
+SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
+
+
+python -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}    --test_report_filename=$REPORT_FILENAME  --cuda
+
+python -m examples.lab --validate_fr  --validate_fr_reportfile=$REPORT_FILENAME  --validate_fr_truthfile=local_data/cifar10/cifar-10-batches-py/test_batch --validate_fr_truthfiletype="pickle_dict"
+
+
+
