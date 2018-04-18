c968a286f647a94d8cb2c88953be9f9999b716a2
paiforsyth <paiforsyth@hotmail.com>
2018-04-18 16:07:50 -0400

    verbose taylor

--- sh/prune_scripts/resnet_normalized_by_layer_global_taylor_prune.sh
+++ sh/prune_scripts/resnet_normalized_by_layer_global_taylor_prune.sh
@@ -1,4 +1,5 @@
-for PRUNE_PCT in 17 20 23 25; do
+for PRUNE_PCT in 15 17 20 do; 
+    #17 20 23 25; do
     #50 60 70 75 80 85 80 95 97; do
  #   0 2 1 5 7 10 12 15 17 20 23 25 27 30 32 35 37 40 42 45 47 50 52 55 57 60 65 70; do
 ARGFILE=./sh/argfiles/resnet29_group
@@ -6,7 +7,7 @@ RESUME_FILENAME=11_April_2018_Wednesday_23_26_51_most_recent
 SAVE_PREFIX=res_net_29_group_prune
 REPORT_FILENAME=./reports/medium_res_net_164_naive_prune_$PRUNE_PCT
 
-python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT  --group_prune_strategy=taylor --prune_layer_mode=global --global_prune_normalization=by_layer --cuda   --output_level=warning --short_test_report   
+python -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX  --resume_mode=standard  --res_file=$RESUME_FILENAME   --prune_trained --prune_trained_pct=$PRUNE_PCT  --group_prune_strategy=taylor --prune_layer_mode=global --global_prune_normalization=by_layer --cuda   --output_level=warning --short_test_report  --verbose_prune_trained 
 
 python  -W ignore -m examples.lab $(cat $ARGFILE) --save_prefix=$SAVE_PREFIX --mode=test --resume_mode=standard  --res_file=${RESUME_FILENAME}_prune_taylor_$PRUNE_PCT   --test_report_filename=$REPORT_FILENAME  --cuda  --output_level=warning
 
