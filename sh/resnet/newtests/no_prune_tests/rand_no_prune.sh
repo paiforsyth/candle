8996642b593900a9e01d3ce25e70866f294f1820
paiforsyth <paiforsyth@hotmail.com>
2018-04-18 12:56:13 -0400

    filename

--- sh/resnet/newtests/no_prune_tests/rand_no_prune.sh
+++ sh/resnet/newtests/no_prune_tests/rand_no_prune.sh
@@ -3,7 +3,7 @@ PTARG=01
 NETTYPE=resnet29
 LAMBDA=00001
 ARGFILE=./sh/argfiles/${NETTYPE}_group
-RESUME_FILENAME=saved_models/09_April_2018_Monday_02_17_27_endofcycle_checkpoint_0
+RESUME_FILENAME=09_April_2018_Monday_02_17_27_endofcycle_checkpoint_0
 SAVE_PREFIX=${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool
 REPORT_FILENAME=./reports/${NETTYPE}_${RUNTYPE}_${PTARG}_lambda${LAMBDA}_test_fixedmaxpool_noprune
 
